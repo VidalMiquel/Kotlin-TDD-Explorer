d3429bb4d322397229a81915419f6f25be70936b
MiquelVidal <miquelvidalcortes@gmail.com>
2025-02-08 12:22:43 +0100

    Created the project strucutre using spring-initializr

---
+++ kotlin-tdd-explorer/src/main/kotlin/com/mvidal/kotlin/tdd/KotlinTddExplorerApplication.kt
@@ -0,0 +1,11 @@
+package com.mvidal.kotlin.tdd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinTddExplorerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinTddExplorerApplication>(*args)
+}
---
+++ kotlin-tdd-explorer/src/test/kotlin/com/mvidal/kotlin/tdd/KotlinTddExplorerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mvidal.kotlin.tdd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinTddExplorerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
